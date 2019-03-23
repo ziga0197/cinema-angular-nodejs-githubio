4418459c28063d8c7fc1026174fe8dc1b82474ac
kiet <hotuankietps02152@gmail.com>
2019-03-23 13:21:50 +0700

    models

---
+++ src/app/Models/Phim.ts
@@ -0,0 +1,10 @@
+export class Phim {
+    DanhGia: number;
+    HinhAnh: string;
+    MaNhom: string;
+    MaPhim: number;
+    MoTa: string;
+    NgayKhoiChieu: string;
+    TenPhim: string;
+    Trailer: string;
+}
