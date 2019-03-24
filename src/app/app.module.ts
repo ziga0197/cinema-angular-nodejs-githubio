40e3974194edfca0b47324754b727608d84bff73
kiet <hotuankietps02152@gmail.com>
2019-03-24 09:06:12 +0700

    apdate app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { HomeModule } from './home/home.module';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+const appRoutes: Routes = [
+  // {path: '', loadChildren: () => HomeModule},
+  {path: 'admin', loadChildren: () => AdminModule}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
