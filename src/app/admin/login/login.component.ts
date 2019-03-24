f0a11ef8e947a9370beb6f06a541467e559272c0
kiet <hotuankietps02152@gmail.com>
2019-03-24 08:59:05 +0700

    login function

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { NguoiDungService } from 'src/app/Services/nguoi-dung/nguoi-dung.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  FormLogin: FormGroup;
+  constructor(private _nguoidungService: NguoiDungService, private _router: Router) {
+  }
 
   ngOnInit() {
+    this.FormLogin = new FormGroup({
+      'taikhoan': new FormControl(null),
+      'matkhau': new FormControl(null)
+    });
+  }
+
+  DangNhap() {
+    this._nguoidungService.DangNhap(this.FormLogin.value).subscribe(
+      (res: any) => {
+        if (typeof res === 'object') {
+          if (res.MaLoaiNguoiDung.toLowerCase() === 'quantri') {
+              swal('Cập Nhật Thành Công', {
+                icon: 'success',
+              }).then((value) => {
+                localStorage.setItem('admin_login', JSON.stringify(res));
+                this._router.navigate(['/admin']);
+              });
+          } else {
+            swal('Người dùng không phải quản trị viên', {
+              icon: 'warning',
+            });
+          }
+        } else {
+          swal(res, {
+            icon: 'error',
+          });
+        }
+      },
+      err => console.log(err)
+    );
   }
 
 }
